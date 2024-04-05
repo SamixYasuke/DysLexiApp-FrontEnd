2a2cd21dd40a056d9194922c8e89bbd826d3db82
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2024-04-05 02:56:55 +0100

    Update UnitContainer.jsx

--- src/components/UnitContainer.jsx
+++ src/components/UnitContainer.jsx
@@ -1,15 +1,32 @@
+// module imports
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+// icon import
+import backBtn from "../assets/images/back btn.png";
+
+// animation import
 import Cloud from "./Cloud";
 
 const UnitContainer = ({ unitId, unitText }) => {
+  const navigate = useNavigate();
   const getUnitText = unitText();
+
   return (
     <div>
       <div>
         <Cloud />
       </div>
-      {unitId && <h2>Unit {unitId}</h2>}
+      <p>
+        <img
+          src={backBtn}
+          alt="backBtn"
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+        />
+        {unitId && <h2>Unit {unitId}</h2>}
+      </p>
       {unitText && <p>{getUnitText}</p>}
     </div>
   );
